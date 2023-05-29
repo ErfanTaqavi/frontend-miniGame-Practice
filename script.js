1a83245b4f074432e2d2fefac395af9525634675
ErfanTaqavi <erfan.taghavi.6280@gmail.com>
2023-05-29 18:07:57 +0330

    add element & func for dice

--- script.js
+++ script.js
@@ -2,8 +2,21 @@
 
 const scoreEl0 = document.getElementById('score--0')   
 const scoreEl1 = document.getElementById('score--1')
-const dice = document.querySelector('.dice')
-
+const diceEl = document.querySelector('.dice')
+const btnNew = document.querySelector('.btn--new') 
+const btnRoll = document.querySelector('.btn--roll')
+const btnHold = document.querySelector('.btn--hold') 
 scoreEl0.textContent = 0
 scoreEl1.textContent = 0
-dice.classList.add('hidden')
+diceEl.classList.add('hidden')
+
+btnRoll.addEventListener('click', function(){
+
+const dice = Math.trunc(Math.random() * 6) + 1;
+
+
+diceEl.classList.remove('hidden')
+diceEl.src = `dice-${dice}.png`
+})
+
+
