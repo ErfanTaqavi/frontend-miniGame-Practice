0cc0d9bdb49976769d4ae984a8dd5f252fdd7845
ErfanTaqavi <erfan.taghavi.6280@gmail.com>
2023-05-29 17:12:24 +0330

    add element score and class hidden for dice in js & csst

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const scoreEl0 = document.getElementById('score--0')   
+const scoreEl1 = document.getElementById('score--1')
+const dice = document.querySelector('.dice')
+
+scoreEl0.textContent = 0
+scoreEl1.textContent = 0
+dice.classList.add('hidden')
