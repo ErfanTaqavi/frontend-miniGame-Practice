5faaf1c74bb9866528e23c308ff5bb0310ef1ca5
ErfanTaqavi <erfan.taghavi.6280@gmail.com>
2023-06-11 18:15:56 +0330

    Maintaining the score of each player and adding the winning rule and disabling the buttons

--- script.js
+++ script.js
@@ -17,33 +17,56 @@ scoreEl1.textContent = 0
 let currentScore = 0
 let scores = [0 , 0]
 let activeplayer = 0
+let playing = true
+
+const switchplayer = function(){
+  
+        document.getElementById(`current--${activeplayer}`).textContent =0
+        currentScore = 0
+        activeplayer = activeplayer === 0 ? 1:0 ;
+        player0El.classList.toggle('player--active')
+        player1El.classList.toggle('player--active')
+   
+}
 
 diceEl.classList.add('hidden')
 
 //Rolling dice function
 btnRoll.addEventListener('click', function(){
     //Gneraiting dice
+    if(playing){
+        const dice = Math.trunc(Math.random() * 6) + 1;
 
-const dice = Math.trunc(Math.random() * 6) + 1;
+        //Display dice
+        diceEl.classList.remove('hidden')
 
-//Display dice
-diceEl.classList.remove('hidden')
+        diceEl.src = `dice-${dice}.png`
 
-diceEl.src = `dice-${dice}.png`
+        //Check for rolled 1
+        if(dice !== 1){
 
-//Check for rolled 1
-if(dice !== 1){
-
-    currentScore += dice
-    document.getElementById(`current--${activeplayer}`).textContent =currentScore
-}else
-{ // switch to next player
-    document.getElementById(`current--${activeplayer}`).textContent =0
-    currentScore = 0
-    activeplayer = activeplayer === 0 ? 1:0 ;
-    player0El.classList.toggle('player--active')
-    player1El.classList.toggle('player--active')
-
+            currentScore += dice
+            document.getElementById(`current--${activeplayer}`).textContent =currentScore
+        }else
+        { // switch to next player
+            switchplayer();
+        }
+    }
+})
+//hold the current score to scores array
+btnHold.addEventListener('click',function(){
+    if(playing){
+    
+    }
+//switch player if player dont give 100 scores else this player winner
+if (scores[activeplayer] >= 10){
+    playing = false
+    document.querySelector(`.player--${activeplayer}`).classList.add('player--winner')
+    document.querySelector(`.player--${activeplayer}`).classList.remove('player--active')
+    diceEl.classList.add('hidden')
+   
+}else{
+    switchplayer();
 }
 })
 
